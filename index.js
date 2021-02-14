8270540f06d0dc66085de4c276460b602106c22d
Bruno Rocha <brunoorochaa@gmail.com>
2021-02-14 18:14:27 -0300

    trocando XMLHttpRequest por fetch

--- index.js
+++ index.js
@@ -3,27 +3,48 @@ document.querySelector("button").addEventListener("click", buscaCep);
 function buscaCep() {
   let inputCep = document.querySelector("input#cep");
   let cep = inputCep.value.replace("-", "");
-  let url = "http://viacep.com.br/ws/" + cep + "/json";
+  let url = `http://viacep.com.br/ws/${cep}/json`;
 
-  let xhr = new XMLHttpRequest();
-  xhr.open("GET", url, true);
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState == 4) {
-      if ((xhr.status = 200)) {
-        completaCampos(JSON.parse(xhr.responseText));
+  // let xhr = new XMLHttpRequest();
+  // xhr.open("GET", url, true);
+  // xhr.onreadystatechange = function () {
+  //   if (xhr.readyState == 4) {
+  //     if ((xhr.status = 200)) {
+  //       completaCampos(JSON.parse(xhr.responseText));
+  //     }
+  //   }
+  // };
+  // xhr.send();
+
+  fetch(url).then((response) => {
+    response.json().then((data) => {
+      if (data.erro) {
+        cepInvalido();
+        return;
       }
-    }
-  };
-  xhr.send();
+      completaCampos(data);
+    });
+  });
+}
+
+function cepInvalido() {
+  alert("CEP invalido");
 }
 
 function completaCampos(endereco) {
-  document.querySelector("#rua").value = endereco.logradouro;
-  document.querySelector("#rua").disabled = true;
-  document.querySelector("#bairro").value = endereco.bairro;
-  document.querySelector("#bairro").disabled = true;
-  document.querySelector("#cidade").value = endereco.localidade;
+  const rua = document.querySelector("#rua");
+  rua.value = endereco.logradouro;
+  rua.disabled = true;
+
+  const bairro = document.querySelector("#bairro");
+  bairro.value = endereco.bairro;
+  bairro.disabled = true;
+
+  const cidade = document.querySelector("#cidade");
+  cidade.value = endereco.localidade;
   document.querySelector("#cidade").disabled = true;
-  document.querySelector("#estado").value = endereco.uf;
-  document.querySelector("#estado").disabled = true;
+
+  const estado = document.querySelector("#estado");
+  estado.value = endereco.uf;
+  estado.disabled = true;
 }
